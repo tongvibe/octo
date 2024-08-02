55681f1d9d748139f2fc6b4d5ef9219d748c6aa7
tongvibe <tong@vibe.us>
2024-08-02 18:16:53 +0800

    slove the Matrix Mask

--- ur5_control_python/python-urx_test.py
+++ ur5_control_python/python-urx_test.py
@@ -3,15 +3,16 @@ import time
 from urx.robotiq_two_finger_gripper import Robotiq_Two_Finger_Gripper
 from urx.urscript import URScript
 from scipy.spatial.transform import Rotation as R  # 导入scipy库中的Rotation类
-
+import numpy as np
+import math
 # Initialize the robot by IP
 rob = urx.Robot("192.168.51.254")
 
-# Set the tool center point (TCP) and payload
-# set the offset and load for robot
+# # Set the tool center point (TCP) and payload
+# # set the offset and load for robot
 
-rob.set_tcp((0, 0, 0, 0, 0, 0))
-rob.set_payload(1, (0, 0, 0.9))
+rob.set_tcp((0, 0, 0.09, 0, 0, 0))
+# rob.set_payload(1, (0, 0, 0.9))
 
 # Allow some time for the robot to process the setup commands
 time.sleep(0.2)
@@ -19,10 +20,96 @@ time.sleep(0.2)
 # Define motion parameters (acceleration and velocity)
 a = 0.05  # example acceleration
 v = 0.05   # example velocity
+#计算
+cam_T_bottle = np.array(
+    [[0.056892, -0.987741, -0.145368, 0.037548],
+    [-0.439035, 0.106018, -0.892193, -0.171583],
+    [0.896667, 0.114581, -0.427621, 0.745482],
+    [0.000000, 0.000000, 0.000000, 1.000000]]
+    )
 
+C= np.array(
+    [[0.056892, -0.987741, -0.145368],
+    [-0.439035, 0.106018, -0.892193],
+    [0.896667, 0.114581, -0.427621]]
+    )
+
+
+Tz = np.array(
+    [[-1,0,0,0],
+     [0,-1,0,0],
+     [0,0,1,0],
+     [0,0,0,1]]
+)
+Ty = np.array(
+    [[0,0,-1],
+     [0,1,0],
+     [1,0,0]]
+)
+
+kk = np.dot(Ty,C)
+Tnb = np.array(
+    [[0,0,1,0],
+     [1,0,0,0],
+     [0,1,0,0],
+     [0,0,0,1]]
+)
+# [0,0,1;
+# 1,0,0;
+# 0,1,0]
+print(kk)
+cam_T_bottle = np.array(
+    [[-0.896667, -0.114581 , 0.427621, 0.037548],
+    [-0.439035 , 0.106018, -0.892193, -0.171583],
+    [ 0.056892 ,-0.987741 ,-0.145368, 0.745482],
+    [0.000000, 0.000000, 0.000000, 1.000000]]
+
+    )
+
+
+# cam_T_bottle = np.dot(Tnb, cam_T_bottle)
+# cam_T_bottle = np.dot(Tx, cam_T_bottle)
+# cam_T_bottle = np.dot(Ty, cam_T_bottle)
+Y = np.array(
+    [[-0.99691752, -0.04396394, -0.06498177, -0.335 ],  #-0.3301283   -0.35202257
+    [ 0.00679575,  0.77674946, -0.62977305,  0.04730107],###0.05730
+    [ 0.07816186, -0.62827339, -0.77405637,  0.66619011],
+    [ 0.    ,      0.     ,     0.     ,     1.        ]]    
+)
+base_T_tag = np.dot(Y, cam_T_bottle)
+
+# base_T_tag = np.array(
+#     [[0.06245558, 0.99176524 , -0.11180769, -0.37611806],
+#     [0.82147682 , -0.11470462,  -0.55858639, -0.26322131],
+#     [0.56681146, 0.05696057, 0.82187608,  0.04590873],
+#     [ 0.      ,    0.        ,  0.      ,    1.        ]]
+#                 )
+
+print("base_T_tag",base_T_tag)
 # Define position/orientation parameters
 # x, y, z = 0.05, 0.05, 0.05  # example positions
 # rx, ry, rz = 0, 0, 0  # example orientations
+def homogeneous_to_rot_trans(T):
+    # 提取平移向量
+    tx, ty, tz = T[0, 3], T[1, 3], T[2, 3]
+    # 提取旋转矩阵
+    R = T[0:3, 0:3]
+    # 计算旋转角度 θ
+    trace_R = np.trace(R)
+    theta = math.acos((trace_R - 1) / 2)
+    if theta < 1e-6:
+        # 如果旋转角度接近0，旋转向量为零向量S
+        return np.array([0, 0, 0]), np.array([tx, ty, tz])
+    # 计算旋转向量
+    rx = (R[2, 1] - R[1, 2]) / (2 * math.sin(theta))
+    ry = (R[0, 2] - R[2, 0]) / (2 * math.sin(theta))
+    rz = (R[1, 0] - R[0, 1]) / (2 * math.sin(theta))
+    rot_vector = theta * np.array([rx, ry, rz])
+    # print(tx,ty,tz,rot_vector)
+    return np.append(np.array([tx, ty, tz]),rot_vector)
+
+ur5_order = homogeneous_to_rot_trans(base_T_tag)
+print(ur5_order)
 
 try:
     # Move to a joint space position
@@ -43,7 +130,7 @@ try:
     print("欧拉角:", euler_angles)
 
     # Move relative to the current pose
-    rob.movel((-0.58479328, -0.46829172 , 0.163,  -0.41, -0.372,  0.139), acc=a, vel=v, relative=False)
+    rob.movel((ur5_order), acc=a, vel=v, relative=False)
     # txt_file = '/home/tong/robotic-ai/octo/examples/action_groundturth.txt'
     # with open(txt_file, 'r') as f:
     #     # 逐行读取数据
